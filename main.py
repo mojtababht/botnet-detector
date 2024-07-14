67369f0d9e503644e320272a7863e7b981ffa83e
mojtaba <mojtababht@gmail.com>
2024-07-14 17:19:27 +0330

    -

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ flows = {}
 def create_flow(pkt: Ether):
     if pkt.haslayer('IP'):
         if pkt.haslayer('UDP'):
-            src_addr = str(pkt[IP].src) + str(pkt[UDP].sport)
-            dst_addr = str(pkt[IP].dst) + str(pkt[UDP].dport)
+            src_addr = str(pkt[IP].src) + ':' + str(pkt[UDP].sport)
+            dst_addr = str(pkt[IP].dst) + ':' + str(pkt[UDP].dport)
             key = (src_addr, dst_addr, 'UDP')
             rev_key = (dst_addr, src_addr, 'UDP')
             if key in flows:
@@ -25,8 +25,8 @@ def create_flow(pkt: Ether):
             else:
                 flows[key] = [pkt]
         elif pkt.haslayer('TCP'):
-            src_addr = str(pkt[IP].src) + str(pkt[TCP].sport)
-            dst_addr = str(pkt[IP].dst) + str(pkt[TCP].dport)
+            src_addr = str(pkt[IP].src) + ':' + str(pkt[TCP].sport)
+            dst_addr = str(pkt[IP].dst) + ':' + str(pkt[TCP].dport)
             key = (src_addr, dst_addr, 'TCP')
             rev_key = (dst_addr, src_addr, 'UDP')
             if key in flows:
@@ -41,13 +41,12 @@ def create_flow(pkt: Ether):
 base_dir = Path()
 pcap_file = base_dir.joinpath('test.pcap')
 
-s = sniff(offline=str(pcap_file), prn=create_flow, count=10000)
-
+s = sniff(offline=str(pcap_file), prn=create_flow)
 
 
 
 for key, val in flows.copy().items():
-    if len(val) <= 2 and key[2] == 'TCP':  # TCP hand shake
+    if len(val) <= 2 and key[2] == 'TCP':  # TCP handshake
         flows.pop(key)
     else:
         val.sort(key=lambda x: x.time)
@@ -58,32 +57,105 @@ for key, val in flows.copy().items():
                 i = val.index(pkt)
                 flows.pop(key)
                 key = list(key)
-                key.append(int(val[previous_index].time.to_integral_value()))
+                key.append(str(val[previous_index].time.to_integral_value()))
                 key = tuple(key)
                 flows[key] = val[previous_index:i]
                 previous_index = i
                 key = list(key)
-                key.append(int(pkt.time.to_integral_value()))
+                key.append(str(pkt.time.to_integral_value()))
                 key = tuple(key)
                 flows[key] = val[i:]
             previous = pkt
 
-flows2 = {}
-max_c = 0
+flow_list = []
+total_time = 0
+total_bytes = 0
+ip_list = []
+
 for key, val in flows.copy().items():
-    pkts = []
-    for pkt in val:
-        pkts.append(str(datetime.fromtimestamp(int(pkt.time.to_integral_value()))))
-    flows2[str(key)] = pkts
-    if len(val) > max_c:
-        max_c = len(val)
+    if len(val) <= 2 and key[2] == 'TCP':  # TCP handshake
+        flows.pop(key)
+    elif len(key) == 3:  # filtering rare connections
+        flows.pop(key)
+    else:
+        packets_bytes = 0
+        for pkt in val:
+            packets_bytes += len(pkt)
+        first_pkt_time = datetime.fromtimestamp(int(val[0].time.to_integral_value()))
+        last_pkt_time = datetime.fromtimestamp(int(val[-1].time.to_integral_value()))
+        diff_time = last_pkt_time - first_pkt_time
+        flow_time = diff_time.total_seconds()
+        total_time += flow_time
+        total_bytes += packets_bytes
+        flow_data = {
+            'src_ip': key[0].split(':')[0],
+            'src_port': key[0].split(':')[-1],
+            'dst_ip': key[1].split(':')[0],
+            'dst_port': key[1].split(':')[-1],
+            'proto': key[2],
+            'first_pkt_timestamp': int(val[0].time.to_integral_value()),
+            'last_pkt_timestamp': int(val[-1].time.to_integral_value()),
+            'packets_bytes': packets_bytes,
+            'flow_time': flow_time,
+            # 'packets': val
+        }
+        ip_list.append(key[0].split(':')[0])
+        ip_list.append(key[1].split(':')[-1])
+        flow_list.append(flow_data)
+
+average_time = total_time / len(flow_list)
+average_bytes = total_bytes / len(flow_list)
+
+for flow in flow_list.copy():  # filtering flows by bytes and time
+    if flow['packets_bytes'] > average_bytes or flow['flow_time'] > average_time:
+        flow_list.remove(flow)
+    elif ip_list.count(flow['src_ip']) <= 1 and ip_list.count(flow['dst_ip']) <= 1:  # filtering flows that has no
+        # common ip with other flows
+        flow_list.remove(flow)
+
+
+flow_list.sort(key=lambda x: x['first_pkt_timestamp'])
+
+dependent_flows = []
+for i in range(len(flow_list)):
+    for j in range(i, len(flow_list)):
+        first_flow_ips = {flow_list[i]['src_ip'], flow_list[i]['dst_ip']}
+        second_flow_ips = {flow_list[j]['src_ip'], flow_list[j]['dst_ip']}
+        if (flow_list[j]['first_pkt_timestamp'] == flow_list[i]['last_pkt_timestamp'] and
+                flow_list[i]['proto'] == flow_list[j]['proto']) and first_flow_ips.intersection(second_flow_ips):
+            dependent_flows.append((flow_list[i], flow_list[j]))
+
+l = []
+for i in dependent_flows.copy():
+    i[0].pop('packets', 1)
+    i[1].pop('packets', 1)
+    try:
+        date = str(datetime.fromtimestamp(i[0]['first_pkt_timestamp']))
+        i[0]['first_pkt_timestamp'] = date
+    except:...
+    try:
+        date = str(datetime.fromtimestamp(i[0]['last_pkt_timestamp']))
+        i[0]['last_pkt_timestamp'] = date
+    except:
+        ...
+    try:
+        date = str(datetime.fromtimestamp(i[-1]['first_pkt_timestamp']))
+        i[1]['first_pkt_timestamp'] = date
+    except:
+        ...
+    try:
+        date = str(datetime.fromtimestamp(i[-1]['last_pkt_timestamp']))
+        i[1]['last_pkt_timestamp'] = date
+    except:
+        ...
+    l.append(i)
+
+import json
 
 with open('res.json', 'w') as f:
-    json.dump(flows2, f)
-
+    json.dump(l, f)
 
 # print(flows)
-print(len(flows), max_c)
 
 
 
